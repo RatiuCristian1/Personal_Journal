ec3ae88175ec92cc1ebfbb1ce2a52a61a3f09525
Ratiu Cristian <lafurnica2008@yahoo.com>
2023-03-21 17:12:06 +0100

    Setting up the display on large devices

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 const viewMoreBtn = document.getElementById('view-more-btn');
 const blogPosts = document.querySelectorAll('.blog-post');
 
-    viewMoreBtn.addEventListener('click', () => {
-        for (let i = blogPosts.length - 1; i >= blogPosts.length - 2; i--) {
-            blogPosts[i].style.display = 'block';
-        }
-        viewMoreBtn.style.display = 'none';
-    });
+viewMoreBtn.addEventListener('click', () => {
+  // Show the last two blog post elements when the button is clicked
+  for (let i = blogPosts.length - 1; i >= blogPosts.length - 2; i--) {
+    blogPosts[i].style.display = 'block';
+  }
+  // Hide the "View More" button after it is clicked
+  viewMoreBtn.style.display = 'none';
+});
