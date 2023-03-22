257b65fb11d536e4ef32a572a54d5141998c8f31
Ratiu Cristian <lafurnica2008@yahoo.com>
2023-03-22 15:37:34 +0100

    Adding 2 more images to the html

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,19 @@ const blogPosts = document.querySelectorAll('.blog-post');
 
 viewMoreBtn.addEventListener('click', () => {
   // Show the last two blog post elements when the button is clicked
-  for (let i = blogPosts.length - 1; i >= blogPosts.length - 2; i--) {
-    blogPosts[i].style.display = 'block';
+  if(blogPosts[0].offsetWidth < 599) {
+    for (let i = blogPosts.length - 1; i >= blogPosts.length - 2; i--) {
+      blogPosts[i].style.display = 'block';
+      blogPosts[i].style.flexDirection = 'row';
+      blogPosts[i].style.gap = '40px'
+    }
+  } else if (blogPosts[0].offsetWidth > 600) {
+    for (let i = blogPosts.length - 1; i >= blogPosts.length - 2; i--) {
+      blogPosts[i].style.display = 'flex';
+      blogPosts[i].style.gap = '40px'
+    }
   }
-  // Hide the "View More" button after it is clicked
   viewMoreBtn.style.display = 'none';
 });
+
+
